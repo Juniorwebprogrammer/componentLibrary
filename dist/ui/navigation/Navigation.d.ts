62e3f42e981f840fc4253d75ab2b40dc2d252269
Junior García <junior.sacarias.garcia.diaz@volenday.com>
2024-09-29 19:31:48 +0100

    clean

--- dist/ui/navigation/Navigation.d.ts
+++
@@ -1,2 +0,0 @@
-import NavigationProps from "./NavigationInterface";
-export default function Navigation({ columns, size, spacing, leftContent, centerContent, rightContent, sxContainer, sxItemLeftContainer, sxItemCenterContainer, sxItemRightContainer }: NavigationProps): import("react").JSX.Element;
